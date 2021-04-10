b8d6a86932eb671fbac9e1c537926687aea5470f
Viktor <viktor_hordubey@yahoo.com>
2021-04-10 21:24:26 +0300

    add video player

--- src/components/VideoSection/Wrapper.tsx
+++ src/components/VideoSection/Wrapper.tsx
@@ -1,5 +1,11 @@
 import styled, { css } from "styled-components";
 
+export const PlayerContainer = styled.div`
+  width: 100%;
+  margin: 0 auto;
+  max-width: 1100px;
+`;
+
 const Wrapper = styled.section`
   padding: 100px 0;
   background: #f3f1fa;
--- src/components/VideoSection/index.tsx
+++ src/components/VideoSection/index.tsx
@@ -1,7 +1,20 @@
-import { FC } from "react";
+import { FC, useRef } from "react";
 
-import Wrapper from "./Wrapper";
+import Wrapper, { PlayerContainer } from "./Wrapper";
 
-const VideoSection: FC = () => <Wrapper></Wrapper>;
+const VideoSection: FC = () => {
+  const ref = useRef<HTMLDivElement | null>(null);
+  return (
+    <Wrapper>
+      <PlayerContainer {...{ ref }}>
+        <div
+          {...{ ref }}
+          className="wave_embed"
+          data-id="6071e15946e0fb000127ae77"
+        />
+      </PlayerContainer>
+    </Wrapper>
+  );
+};
 
 export default VideoSection;

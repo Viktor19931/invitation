c800accf303a95ca9a0f1eb3df1050176396ffb1
Viktor <viktor_hordubey@yahoo.com>
2021-04-10 20:36:13 +0300

    add planing section

---
+++ src/components/Planning/index.tsx
@@ -0,0 +1,44 @@
+import { FC } from "react";
+
+import Wrapper, {
+  Card,
+  CardItem,
+  Title,
+  EventDate,
+  AddressTitle,
+  AddressValue,
+} from "./Wrapper";
+import { SectionTitle, MainSubtitle } from "../../styles/helpers";
+
+const Planning: FC = () => {
+  return (
+    <Wrapper>
+      <MainSubtitle>РОЗКЛАД</MainSubtitle>
+      <SectionTitle>Де і Коли</SectionTitle>
+      <Card>
+        <CardItem>
+          <Title>Церемонія вінчання</Title>
+          <EventDate>30/07/2021 13:00</EventDate>
+
+          <AddressTitle>Address</AddressTitle>
+          <EventDate>
+            Jl. Cempaka 2882, Tegalsari <br />
+            Surabaya, Jawa Timur
+          </EventDate>
+        </CardItem>
+        <CardItem>
+          <Title>Святковий бенкет</Title>
+          <EventDate>30/07/2021 17:00</EventDate>
+
+          <AddressTitle>Address</AddressTitle>
+          <EventDate>
+            Jl. Mayjend Sungkono 212, <br />
+            Surabaya, Jawa Timur
+          </EventDate>
+        </CardItem>
+      </Card>
+    </Wrapper>
+  );
+};
+
+export default Planning;
